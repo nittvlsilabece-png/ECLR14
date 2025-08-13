fc2eab71421b4d8e4787cb011fb4362994f1353f
venkataAjay55 <91135333+venkataAjay55@users.noreply.github.com>
2025-08-13 15:28:47 +0530

    Update jk_tb.v

--- flip flops/jk_tb.v
+++ flip flops/jk_tb.v
@@ -1,1 +1,15 @@
-
+module JK_FF_tb;
+    reg J, K, clk, rst_n;
+    wire Q, Qbar;
+    JK_FF dut(.J(J), .K(K), .clk(clk), .rst_n(rst_n), .Q(Q), .Qbar(Qbar));
+    initial begin clk = 0; forever #5 clk = ~clk; end
+    initial begin
+        rst_n = 0; J=0; K=0; #12;
+        rst_n = 1;
+        J=0; K=0; #10;
+        J=1; K=0; #10;
+        J=0; K=1; #10;
+        J=1; K=1; #20;
+        $finish;
+    end
+endmodule
