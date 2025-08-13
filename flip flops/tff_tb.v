a6eaf0b1e6484e4b2a1e818f5bdfdf4d1c60522c
venkataAjay55 <91135333+venkataAjay55@users.noreply.github.com>
2025-08-13 15:33:02 +0530

    Update tff_tb.v

--- flip flops/tff_tb.v
+++ flip flops/tff_tb.v
@@ -1,1 +1,15 @@
-
+module T_FF_tb;
+    reg T, clk, rst_n;
+    wire Q, Qbar;
+    T_FF dut(.T(T), .clk(clk), .rst_n(rst_n), .Q(Q), .Qbar(Qbar));
+    initial begin clk = 0; forever #5 clk = ~clk; end
+    initial begin
+        rst_n = 0; T=0; #12;
+        rst_n = 1;
+        T = 0; #10;
+        T = 1; #10;
+        T = 1; #10;
+        T = 0; #10;
+        $finish;
+    end
+endmodule
