630020b11b78e425bdb38ed68eaeebf6cb53dfb2
venkataAjay55 <91135333+venkataAjay55@users.noreply.github.com>
2025-08-13 15:46:23 +0530

    Update pipo.v

--- Shift Registers/pipo.v
+++ Shift Registers/pipo.v
@@ -1,1 +1,13 @@
+module pipo #(parameter N = 4) (
+    input  clk, reset,
+    input  [N-1:0] parallel_in,
+    output reg [N-1:0] parallel_out
+);
+    always @(posedge clk or posedge reset) begin
+        if (reset)
+            parallel_out <= 0;
+        else
+            parallel_out <= parallel_in;
+    end
+endmodule
 
