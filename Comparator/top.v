631a678ebe8452c31e57e7a8e2d667388e80c7ac
venkataAjay55 <91135333+venkataAjay55@users.noreply.github.com>
2025-08-13 14:38:25 +0530

    Update and rename test.v to top.v

---
+++ Comparator/top.v
@@ -0,0 +1,14 @@
+module Comparator_4bit (
+    input [3:0] A,
+    input [3:0] B,
+    output A_gt_B,
+    output A_lt_B,
+    output A_eq_B
+);
+
+// Using logic expressions
+assign A_gt_B = (A > B);
+assign A_lt_B = (A < B);
+assign A_eq_B = (A == B);
+
+endmodule
