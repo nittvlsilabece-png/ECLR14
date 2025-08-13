b1250486e51e69e8ef2e9bc11b8ce7e69efa57d3
venkataAjay55 <91135333+venkataAjay55@users.noreply.github.com>
2025-08-13 15:31:05 +0530

    Update dff.v

--- flip flops/dff.v
+++ flip flops/dff.v
@@ -1,1 +1,16 @@
+module D_FF (
+    input  wire D,
+    input  wire clk,
+    input  wire rst_n,
+    output reg  Q,
+    output wire Qbar
+);
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n)
+            Q <= 1'b0;
+        else
+            Q <= D;
+    end
+    assign Qbar = ~Q;
+endmodule
 
