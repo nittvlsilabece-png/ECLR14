8582ee7fc0e0cdef01d22c3d9c27b589e323dce7
venkataAjay55 <91135333+venkataAjay55@users.noreply.github.com>
2025-08-13 15:41:20 +0530

    Update siso_tb.v

--- Shift Registers/siso_tb.v
+++ Shift Registers/siso_tb.v
@@ -1,1 +1,23 @@
+`timescale 1ns/1ps
+module tb_siso;
+    reg clk, reset, serial_in;
+    wire serial_out;
+
+    siso #(.N(4)) uut (.clk(clk), .reset(reset), .serial_in(serial_in), .serial_out(serial_out));
+
+    always #5 clk = ~clk; // 100 MHz clock
+
+    initial begin
+        clk = 0; reset = 1; serial_in = 0;
+        #10 reset = 0;
+
+        serial_in = 1; #10;
+        serial_in = 0; #10;
+        serial_in = 1; #10;
+        serial_in = 1; #10;
+        serial_in = 0; #10;
+
+        $finish;
+    end
+endmodule
 
