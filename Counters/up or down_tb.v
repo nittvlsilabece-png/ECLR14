2edacac6bb6bc9316d35fb85cc4244e47882d4dd
venkataAjay55 <91135333+venkataAjay55@users.noreply.github.com>
2025-08-13 23:59:49 +0530

    Update up or down_tb.v

--- Counters/up or down_tb.v
+++ Counters/up or down_tb.v
@@ -1,1 +1,22 @@
-
+module updowncounter_testbench();
+    reg clk, reset,up_down;
+    wire [3:0] counter;
+    
+    up_down_counter dut(clk, reset,up_down, counter);
+    initial begin 
+        clk=0;
+        forever #5 clk=~clk;
+    end
+    initial begin
+        reset=1;
+        up_down=0;
+        #20;
+        reset=0;
+        #200;
+        up_down=1;
+      #200;
+      up_down=0;
+      #200;
+      $finish;
+    end
+endmodule 
