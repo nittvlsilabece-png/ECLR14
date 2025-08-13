34289675b1031d974f2c6710b693c5a477c46450
venkataAjay55 <91135333+venkataAjay55@users.noreply.github.com>
2025-08-13 15:01:16 +0530

    Create 4to1_mux.v

---
+++ mux and demux/4to1_mux.v
@@ -0,0 +1,14 @@
+module mux4to1 (
+    input  [3:0] in,   // 4 bit input 
+    input  [1:0] sel,  // select lines
+    output reg out
+);
+    always @(*) begin
+        case (sel)
+            2'b00: out = in[0];
+            2'b01: out = in[1];
+            2'b10: out = in[2];
+            2'b11: out = in[3];
+        endcase
+    end
+endmodule
