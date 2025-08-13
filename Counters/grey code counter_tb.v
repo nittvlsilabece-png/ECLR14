eb721225ad7a5fc87170445603fc8afe015c3bfe
venkataAjay55 <91135333+venkataAjay55@users.noreply.github.com>
2025-08-13 23:41:46 +0530

    Update grey code counter_tb.v

--- Counters/grey code counter_tb.v
+++ Counters/grey code counter_tb.v
@@ -1,1 +1,28 @@
+`timescale 1ns/1ps
+module tb_gray_counter;
 
+    reg clk;
+    reg rst;
+    wire [3:0] gray;
+
+    // Instantiate DUT
+    gray_counter uut (
+        .clk(clk),
+        .rst(rst),
+        .gray(gray)
+    );
+
+    // Generate clock with 10ns period
+    initial clk = 0;
+    always #5 clk = ~clk;
+
+    initial begin
+        // Apply reset
+        rst = 1;
+        #12 rst = 0;
+
+        // Run for 200ns
+        #200;
+        $stop;
+    end
+endmodule
