15db90fa59952588b88ef0712315435b8119a5ff
venkataAjay55 <91135333+venkataAjay55@users.noreply.github.com>
2025-08-13 15:32:14 +0530

    Update tff.v

--- flip flops/tff.v
+++ flip flops/tff.v
@@ -1,1 +1,17 @@
-
+module T_FF (
+    input  wire T,
+    input  wire clk,
+    input  wire rst_n,
+    output reg  Q,
+    output wire Qbar
+);
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n)
+            Q <= 1'b0;
+        else if (T)
+            Q <= ~Q;
+        else
+            Q <= Q; // hold
+    end
+    assign Qbar = ~Q;
+endmodule
