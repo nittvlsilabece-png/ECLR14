389d4706f1effdfdc12d5de0340a09a22993ba2c
venkataAjay55 <91135333+venkataAjay55@users.noreply.github.com>
2025-08-14 00:24:13 +0530

    Update sync_up_tb.v

--- Counters/sync_up_tb.v
+++ Counters/sync_up_tb.v
@@ -1,1 +1,29 @@
+`timescale 1ns/1ps
+module tb_sync_up_counter_jk;
+    reg clk, reset;
+    wire [3:0] q;
+
+    // Instantiate counter
+    sync_up_counter_jk uut (
+        .clk(clk),
+        .reset(reset),
+        .q(q)
+    );
+
+    // Clock generation
+    initial begin
+        clk = 0;
+        forever #5 clk = ~clk; // 10 ns period
+    end
+
+    // Stimulus
+    initial begin
+
+        reset = 1;
+        #12 reset = 0;  // release reset
+
+        #200; // run for some time
+        $finish;
+    end
+endmodule
 
