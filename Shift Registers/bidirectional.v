c39c603657da708b423cc6aa7a7e5cb6570b17e5
venkataAjay55 <91135333+venkataAjay55@users.noreply.github.com>
2025-08-13 15:50:55 +0530

    Update bidirectional.v

--- Shift Registers/bidirectional.v
+++ Shift Registers/bidirectional.v
@@ -1,1 +1,15 @@
+module bidir_shift_reg #(parameter N = 4) (
+    input  clk, reset, dir, // dir=0 -> right, dir=1 -> left
+    input  serial_in,
+    output reg [N-1:0] q
+);
+    always @(posedge clk or posedge reset) begin
+        if (reset)
+            q <= 0;
+        else if (dir == 1'b0)
+            q <= {serial_in, q[N-1:1]}; // shift right
+        else
+            q <= {q[N-2:0], serial_in}; // shift left
+    end
+endmodule
 
