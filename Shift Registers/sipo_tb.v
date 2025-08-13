ecad74b3c140baf7abd40cf54f286076238414f2
venkataAjay55 <91135333+venkataAjay55@users.noreply.github.com>
2025-08-13 15:43:18 +0530

    Update sipo_tb.v

--- Shift Registers/sipo_tb.v
+++ Shift Registers/sipo_tb.v
@@ -1,1 +1,22 @@
+`timescale 1ns/1ps
+module tb_sipo;
+    reg clk, reset, serial_in;
+    wire [3:0] parallel_out;
+
+    sipo #(.N(4)) uut (.clk(clk), .reset(reset), .serial_in(serial_in), .parallel_out(parallel_out));
+
+    always #5 clk = ~clk;
+
+    initial begin
+        clk = 0; reset = 1; serial_in = 0;
+        #10 reset = 0;
+
+        serial_in = 1; #10;
+        serial_in = 0; #10;
+        serial_in = 1; #10;
+        serial_in = 1; #10;
+
+        $finish;
+    end
+endmodule
 
