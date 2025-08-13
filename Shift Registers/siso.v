8ecdd15c058038fb722ae09325b16dd1bfb421b0
venkataAjay55 <91135333+venkataAjay55@users.noreply.github.com>
2025-08-13 15:41:01 +0530

    Update siso.v

--- Shift Registers/siso.v
+++ Shift Registers/siso.v
@@ -1,1 +1,16 @@
+module siso #(parameter N = 4) (
+    input  clk, reset, serial_in,
+    output serial_out
+);
+    reg [N-1:0] shift_reg;
+
+    always @(posedge clk or posedge reset) begin
+        if (reset)
+            shift_reg <= 0;
+        else
+            shift_reg <= {shift_reg[N-2:0], serial_in};
+    end
+
+    assign serial_out = shift_reg[N-1];
+endmodule
 
