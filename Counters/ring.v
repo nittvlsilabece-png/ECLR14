798d41332ba1bd6e6704144c2f41fa9b911b4275
venkataAjay55 <91135333+venkataAjay55@users.noreply.github.com>
2025-08-13 23:47:59 +0530

    Update ring.v

--- Counters/ring.v
+++ Counters/ring.v
@@ -1,1 +1,14 @@
+module ring_counter(
+    input  wire clk,
+    input  wire rst,      // Active high reset
+    output reg  [3:0] q
+);
 
+    always @(posedge clk or posedge rst) begin
+        if (rst)
+            q <= 4'b0001;            // Start from 0001
+        else
+            q <= {q[2:0], q[3]};     // Rotate left
+    end
+
+endmodule
