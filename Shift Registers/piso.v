e7bccac4e2355850e68914e68f25b2646c912ad9
venkataAjay55 <91135333+venkataAjay55@users.noreply.github.com>
2025-08-13 15:44:58 +0530

    Update piso.v

--- Shift Registers/piso.v
+++ Shift Registers/piso.v
@@ -1,1 +1,19 @@
+module piso #(parameter N = 4) (
+    input  clk, reset, load,
+    input  [N-1:0] parallel_in,
+    output serial_out
+);
+    reg [N-1:0] shift_reg;
+
+    always @(posedge clk or posedge reset) begin
+        if (reset)
+            shift_reg <= 0;
+        else if (load)
+            shift_reg <= parallel_in;
+        else
+            shift_reg <= {1'b0, shift_reg[N-1:1]};
+    end
+
+    assign serial_out = shift_reg[0];
+endmodule
 
