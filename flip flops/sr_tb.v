98a1e5b97b720fb2e6f2aab702c5a9bfb3c15975
venkataAjay55 <91135333+venkataAjay55@users.noreply.github.com>
2025-08-13 15:26:31 +0530

    Update sr_tb.v

--- flip flops/sr_tb.v
+++ flip flops/sr_tb.v
@@ -1,1 +1,16 @@
-
+module SR_FF_tb;
+    reg S, R, clk, rst_n;
+    wire Q, Qbar;
+    SR_FF dut(.S(S), .R(R), .clk(clk), .rst_n(rst_n), .Q(Q), .Qbar(Qbar));
+    initial begin clk = 0; forever #5 clk = ~clk; end
+    initial begin
+        rst_n = 0; S=0; R=0; #12;
+        rst_n = 1;
+        S=0; R=0; #10;
+        S=1; R=0; #10;
+        S=0; R=1; #10;
+        S=1; R=1; #10;
+        S=0; R=0; #10;
+        $finish;
+    end
+endmodule
