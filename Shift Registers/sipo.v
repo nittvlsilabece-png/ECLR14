244475335f1b0be23550b867705ddeb40df57bc1
venkataAjay55 <91135333+venkataAjay55@users.noreply.github.com>
2025-08-13 15:42:56 +0530

    Update sipo.v

--- Shift Registers/sipo.v
+++ Shift Registers/sipo.v
@@ -1,1 +1,16 @@
+module sipo #(parameter N = 4) (
+    input  clk, reset, serial_in,
+    output [N-1:0] parallel_out
+);
+    reg [N-1:0] shift_reg;
+
+    always @(posedge clk or posedge reset) begin
+        if (reset)
+            shift_reg <= 0;
+        else
+            shift_reg <= {shift_reg[N-2:0], serial_in};
+    end
+
+    assign parallel_out = shift_reg;
+endmodule
 
