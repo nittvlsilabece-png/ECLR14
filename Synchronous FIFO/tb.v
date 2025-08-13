66f11ec184293617eccfeedc3c54dfaf161aa45b
venkataAjay55 <91135333+venkataAjay55@users.noreply.github.com>
2025-08-13 18:54:54 +0530

    Update tb.v

--- Synchronous FIFO/tb.v
+++ Synchronous FIFO/tb.v
@@ -1,26 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 13.08.2025 17:00:18
-// Design Name: 
-// Module Name: tb_fifo
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-
 
 module tb_synchronous_fifo;
 
