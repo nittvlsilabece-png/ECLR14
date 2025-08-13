7256f6f598b451bd1cf8d8717f40d5aa7e1128bf
venkataAjay55 <91135333+venkataAjay55@users.noreply.github.com>
2025-08-13 23:41:25 +0530

    Update grey code counter.v

--- Counters/grey code counter.v
+++ Counters/grey code counter.v
@@ -1,1 +1,22 @@
 
+module gray_counter(
+    input  wire clk,
+    input  wire rst,        // Active high reset
+    output reg [3:0] gray
+);
+
+    reg [3:0] count;        // Normal binary counter
+
+    always @(posedge clk or posedge rst) begin
+        if (rst)
+            count <= 4'b0000;
+        else
+            count <= count + 1;
+    end
+
+    // Combinational logic to convert binary -> Gray code
+    always @(*) begin
+        gray = count ^ (count >> 1);
+    end
+
+endmodule
