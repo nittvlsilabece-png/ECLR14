32c2da4cb38b0210d6a374f57b1fbba94d388222
venkataAjay55 <91135333+venkataAjay55@users.noreply.github.com>
2025-08-14 00:18:30 +0530

    Update async_down_tb.v

--- Counters/async_down_tb.v
+++ Counters/async_down_tb.v
@@ -1,1 +1,19 @@
+module tb;
+  reg clk, rst_n;
+  reg j, k;
+  wire [3:0] q, q_bar;
+  asynchronous_counter dut(clk, rst_n, j, k, q, q_bar);
+  
+  initial begin
+    clk = 0; rst_n = 0; 
+    #4; rst_n = 1;
+    j = 1; k = 1;
+    #80;
+    rst_n = 0;
+    #4; rst_n = 1;
+    #50;
+    $finish;
+  end
+  always #2 clk = ~clk;
 
+endmodule
